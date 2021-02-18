984c1cc9f92c889acf1e1ec0d4884c48326c5329
pangnasun <47574691+pangnasun@users.noreply.github.com>
2021-02-18 22:04:17 +0400

    animations

---
+++ Assets/Church1EndB1Controller.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Church1EndB1Controller : MonoBehaviour
+{
+    private Animator animator;
+    private bool stopState;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        stopState = true;
+    }
+
+    public void OnPointerClick()
+    {
+
+
+        if (animator.GetFloat("AnimSpeed") == 0f && stopState)
+        {
+            stopState = false;
+            animator.SetFloat("AnimSpeed", 1f);
+            animator.SetTrigger("Play");
+        }
+        else if (animator.GetFloat("AnimSpeed") == 1f)
+        {
+            stopState = true;
+            animator.SetFloat("AnimSpeed", 0f);
+            animator.SetTrigger("Stop");
+        }
+
+
+
+    }
+
+    void Update()
+    {
+        if (IsDone(animator, "BLayer.Church1EndB1Anim") && !stopState)
+        {
+            Debug.Log("running");
+            stopState = true;
+            animator.SetFloat("AnimSpeed", 0f);
+            animator.SetTrigger("Stop");
+        }
+
+
+
+
+    }
+
+
+    bool IsDone(Animator anim, string stateName)
+    {
+        if (anim.GetCurrentAnimatorStateInfo(1).IsName(stateName) &&
+                anim.GetCurrentAnimatorStateInfo(1).normalizedTime > 0.999f)
+            return true;
+        else
+            return false;
+    }
+}
